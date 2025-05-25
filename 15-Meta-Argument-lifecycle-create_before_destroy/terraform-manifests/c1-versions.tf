74d36f2b9dce7df18d7de0a50860aaa62413c790
Dheeraj Negi <a1dheerajnegi@gmail.com>
2025-05-25 11:02:59 +0530

    terrafrom create and destroy

---
+++ 15-Meta-Argument-lifecycle-create_before_destroy/terraform-manifests/c1-versions.tf
@@ -0,0 +1,15 @@
+terraform {
+  required_version = ">= 1.11.4"
+  required_providers {
+    azurerm = {
+        source = "hashicorp/azurerm"
+        version = ">= 1.11.4"
+    }
+  }
+}
+
+provider "azurerm" {
+    features {}
+    subscription_id = "08607c6d-1d61-4140-a717-47cad9c37fc8" 
+  
+}
---
+++ 15-Meta-Argument-lifecycle-create_before_destroy/terraform-manifests/c3-virtual-network.tf
@@ -0,0 +1,11 @@
+resource "azurerm_virtual_network" "myvnet" {
+    name = "myvnet-01"
+    # name = "myvnet-02"
+    location = azurerm_resource_group.myrg.location
+    address_space = [ "10.0.0.0/16" ]
+    resource_group_name = azurerm_resource_group.myrg.name
+    lifecycle {
+      create_before_destroy = true
+    }
+  
+}
