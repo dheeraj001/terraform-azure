24c4bd0f07be391493616f074e84fb8f4361ceb4
Dheeraj Negi <a1dheerajnegi@gmail.com>
2025-05-28 17:30:54 +0530

    input variable with list

---
+++ 26-Input-Variables-Collection-Type-Maps/terraform-manifests/c2-variables.tf
@@ -0,0 +1,61 @@
+variable "business_unit" {
+  description = "Business unit name"
+  type        = string
+  default     = "hr"
+}
+
+variable "Environment" {
+  description = "Environment Name"
+  type        = string
+  default     = "dev"
+}
+
+variable "resource_group_name" {
+  description = "Name of resource group"
+  type        = string
+  default     = "myrg"
+}
+
+variable "resource_group_location" {
+  description = "Resouce grouo location"
+  type        = string
+  default     = "eastus2"
+}
+
+variable "virtual_network_name" {
+  description = "Virtual Network Name"
+  type        = string
+  default     = "myvnet"
+
+}
+
+variable "virtual_subnet" {
+  description = "Virtual Network subnet name"
+  type        = string
+}
+
+variable "virtual_network_address_space" {
+  description = "Virtual Network  Address Space"
+  type        = list(string)
+  default     = ["10.0.0.0/16", "10.1.0.0/16", "10.2.0.0/16"]
+
+}
+
+variable "public_ip_sku" {
+  description = "Azure public ip address sku"
+  type        = map(string)
+  default = {
+    "eastus"  = "Basic",
+    "eastus2" = "Standard"
+  }
+}
+
+variable "common_tags" {
+  description = "Common tags for azure resource"
+  type        = map(string)
+  default = {
+    "CLITool" = "Terraform",
+    "Tag1"    = "Azure"
+  }
+
+}
---
+++ 26-Input-Variables-Collection-Type-Maps/terraform-manifests/terraform.tfvars
@@ -0,0 +1,7 @@
+business_unit                 = "it"
+resource_group_name           = "rg"
+virtual_network_name          = "vnet"
+virtual_subnet                = "subnet"
+Environment                   = "dev01"
+resource_group_location       = "eastus2"
+virtual_network_address_space = ["10.3.0.0/16", "10.4.0.0/16", "10.5.0.0/16"]
