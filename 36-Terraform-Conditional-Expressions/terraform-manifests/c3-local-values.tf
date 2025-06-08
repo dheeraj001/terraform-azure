f08fac51015091742ab5b13c5d53e05ac7eb711b
Dheeraj Negi <a1dheerajnegi@gmail.com>
2025-06-08 09:35:56 +0530

    Refer local variable in terraform

---
+++ 36-Terraform-Conditional-Expressions/terraform-manifests/c3-local-values.tf
@@ -0,0 +1,12 @@
+locals {
+  rg_name   = "${var.business_unit}-${var.environment}-${var.resoure_group_name}"
+  vnet_name = "${var.business_unit}-${var.environment}-${var.virtual_network_name}"
+
+  service_name = "Demo Service"
+  owner        = "Dheeraj Negi"
+  common_tags = {
+    Service = local.service_name
+    Owner   = local.owner
+  }
+  vnet_address_space = (var.environment == "dev" ? var.virtual_address_space_dev : var.virtual_address_space_all )
+}
