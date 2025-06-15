03df4bd3bf2c96eceff83b4e5236159f6eed85cf
Dheeraj Negi <a1dheerajnegi@gmail.com>
2025-06-15 10:24:43 +0530

    terrafrom auto refersh

---
+++ 41-Terraform-apply-refresh-only-command/terraform-manifests/c1-versions.tf
@@ -0,0 +1,18 @@
+# Terraform Block
+terraform {
+  required_version = ">= 1.0.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 2.0"
+    }
+  }
+}
+
+# Provider Block
+provider "azurerm" {
+  features {}
+  subscription_id = "08607c6d-1d61-4140-a717-47cad9c37fc8"
+}
+
+
